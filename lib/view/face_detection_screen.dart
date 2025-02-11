89d1680bdd3992df31d769c612382c81bb0fd112
developerSajib88 <developersajib88@gmail.com>
2025-02-11 10:46:55 +0600

    [PACKAGE] Face Live ness detection package has installed.

---
+++ lib/view/face_detection_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class FaceDetectionScreen extends StatefulWidget {
+  const FaceDetectionScreen({super.key});
+
+  @override
+  State<FaceDetectionScreen> createState() => _FaceDetectionScreenState();
+}
+
+class _FaceDetectionScreenState extends State<FaceDetectionScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      child: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
